850d5577193e6eab4ef7b339dbb1fe7c630bd453
Max Silvers <maxsilv@umich.edu>
2023-02-03 13:50:57 -0500

    aws testing

--- ecs_devops_sandbox_cdk/ecs_devops_sandbox_cdk_stack.py
+++ ecs_devops_sandbox_cdk/ecs_devops_sandbox_cdk_stack.py
@@ -1,19 +1,59 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+"""AWS CDK module to create ECS infrastructure"""
 from constructs import Construct
+from aws_cdk import Stack
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_ecr as ecr
+from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_iam as iam
 
 class EcsDevopsSandboxCdkStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create the ECR Repository
+        ecr_repository = ecr.Repository(self,
+                                        "ecs-devops-sandbox-repository",
+                                        repository_name="ecs-devops-sandbox-repository")
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EcsDevopsSandboxCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create the ECS Cluster (and VPC)
+        vpc = ec2.Vpc(self,
+                      "ecs-devops-sandbox-vpc",
+                      max_azs=3)
+        cluster = ecs.Cluster(self,
+                              "ecs-devops-sandbox-cluster",
+                              cluster_name="ecs-devops-sandbox-cluster",
+                              vpc=vpc)
+
+        # Create the ECS Task Definition with placeholder container (and named Task Execution IAM Role)
+        execution_role = iam.Role(self,
+                                  "ecs-devops-sandbox-execution-role",
+                                  assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+                                  role_name="ecs-devops-sandbox-execution-role")
+        execution_role.add_to_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            resources=["*"],
+            actions=[
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+                ]
+        ))
+        task_definition = ecs.FargateTaskDefinition(self,
+                                                    "ecs-devops-sandbox-task-definition",
+                                                    execution_role=execution_role,
+                                                    family="ecs-devops-sandbox-task-definition")
+        container = task_definition.add_container(
+            "ecs-devops-sandbox",
+            image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")
+        )
+
+        # Create the ECS Service
+        service = ecs.FargateService(self,
+                                     "ecs-devops-sandbox-service",
+                                     cluster=cluster,
+                                     task_definition=task_definition,
+                                     service_name="ecs-devops-sandbox-service")
